f3c86834819fae98f8e97198c8d1680339f72b88
Italo Simões <2d.italo@gmail.com>
2023-07-03 08:37:36 -0300

    adição da seção de servicos com imagens

--- src/components/Servicos/Servicos.jsx
+++ src/components/Servicos/Servicos.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Container } from "@mui/material";
+import "./style.scss";
+import { BusinessCenter } from "@mui/icons-material";
+import Servicos01 from "../../assets/servico-img01.png";
+import Servicos02 from "../../assets/servico-img02.png";
 
 const Servicos = () => {
   return (
-    <div>Servicos</div>
-  )
-}
+    <div className="servicos-wrapper">
+      <Container>
+        <h1>Serviços Oferecidos</h1>
+        <hr className="servicos-divider" />
 
-export default Servicos
+        <div className="servicos-wrapper-content">
+          <BusinessCenter className="servicos-icon" />
+          <h2>Serviço 01</h2>
+          <div>
+            <img className="servicos-thumb" src={Servicos01} alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              consequatur optio illo repellat sint autem animi nisi a accusamus
+              exercitationem, reiciendis qui pariatur saepe dolores ut
+              praesentium eius quae provident.
+            </p>
+          </div>
+        </div>
+        <hr className="servicos-divider" />
+
+        <div className="servicos-wrapper-content">
+          <h2>Serviço 02</h2>
+          <div>
+            <img className="servicos-thumb" src={Servicos02} alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              consequatur optio illo repellat sint autem animi nisi a accusamus
+              exercitationem, reiciendis qui pariatur saepe dolores ut
+              praesentium eius quae provident.
+            </p>
+          </div>
+        </div>     
+       
+        <hr className="servicos-divider" />
+      </Container>
+    </div>
+  );
+};
+
+export default Servicos;
